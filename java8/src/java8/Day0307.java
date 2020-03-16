4586fbe8eb9600599a97b3dd5522278074c13158
SinJaeWon <yn-s02@DESKTOP-LGKMUIJ>
2020-03-16 14:56:27 +0900

    3일차: 대입연산자

---
+++ java8/src/java8/Day0307.java
@@ -0,0 +1,24 @@
+package java8;
+
+public class Day0307 {
+	public static void main(String[] args) {
+	
+		int a = 10;
+		int b = 3;
+		a += b; // a = a+b
+		System.out.println(a);
+		a -= b; // a = a-b;
+		System.out.println(a);
+		a *= b;
+		System.out.println(a);
+	
+		a = 10;
+		System.out.println(a);
+		
+		a *= 3+10; //대입연산자는 오른쪽부터 그리고 맨 마지막에 해야하는것.** a = a * 3+10
+		System.out.println(a);
+		
+		a += 1;
+		System.out.println(a);
+	}
+}
