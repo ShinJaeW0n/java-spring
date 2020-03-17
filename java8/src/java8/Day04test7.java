368a94b16b3fa98780883a8690ec4ed21893bd56
SinJaeWon <yn-s02@DESKTOP-LGKMUIJ>
2020-03-18 08:52:33 +0900

    4일차:테스트7

--- java8/src/java8/Day04test7.java
+++ java8/src/java8/Day04test7.java
@@ -22,14 +22,15 @@ public class Day04test7 {
 		
 		//값을 교환하는 swap code가 필요. (n =10, m=20으로 가정)
 		//n = m;
-		//m = n;
+		//m = n; <-이렇게 출력되면 m이 n과 똑같은 수가 나온다.
+		
 		temp = n;
 		n = m;
 		m = temp;
 		
 		//주로 오름차순, 내림차순같은 정렬에 사용됨.**
 		
-		System.out.println("교환 전: M = " + m + "  , N = " + n);
+		System.out.println("교환 후: M = " + m + "  , N = " + n);
 	}
 
 }
