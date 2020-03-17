b55bf67593928888fbeb91a49ed2eb1f23ce4023
SinJaeWon <yn-s02@DESKTOP-LGKMUIJ>
2020-03-17 16:28:58 +0900

    4일차: 테스트7

---
+++ java8/src/java8/Day04test7.java
@@ -0,0 +1,35 @@
+package java8;
+
+import java.util.Scanner;
+
+public class Day04test7 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n , m;
+		int temp;
+		
+		
+		System.out.println("입력N: ");
+		n = sc.nextInt();
+		
+		System.out.println("입력M: ");
+		m = sc.nextInt();
+		
+		System.out.println("교환 전: N = " + n + "  , M = " + m);
+		
+		
+		//값을 교환하는 swap code가 필요. (n =10, m=20으로 가정)
+		//n = m;
+		//m = n;
+		temp = n;
+		n = m;
+		m = temp;
+		
+		//주로 오름차순, 내림차순같은 정렬에 사용됨.**
+		
+		System.out.println("교환 전: M = " + m + "  , N = " + n);
+	}
+
+}
