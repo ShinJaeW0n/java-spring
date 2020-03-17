5b62021bbca1e363567d23ba9f023323af50750a
SinJaeWon <yn-s02@DESKTOP-LGKMUIJ>
2020-03-17 14:49:20 +0900

    xx

--- java8/src/java8/Day0307.java
+++ java8/src/java8/Day0307.java
@@ -7,18 +7,19 @@ public class Day0307 {
 		int b = 3;
 		a += b; // a = a+b
 		System.out.println(a);
-		a -= b; // a = a-b;
+		a -= b; // a = a-b;  13-3
 		System.out.println(a);
 		a *= b;
 		System.out.println(a);
 	
-		a = 10;
+		a = 10; // 다시 값 초기화.
 		System.out.println(a);
 		
 		a *= 3+10; //대입연산자는 오른쪽부터 그리고 맨 마지막에 해야하는것.** a = a * 3+10
 		System.out.println(a);
 		
-		a += 1;
+		a += 1; //131이 나옴.
 		System.out.println(a);
+		
 	}
 }
