b39cf0b34c7c02f6381428c28226ba5531d6c0d7
SinJaeWon <yn-s02@DESKTOP-LGKMUIJ>
2020-03-20 16:32:51 +0900

    자바6일차: continue문

---
+++ java8/src/java8/Day0613.java
@@ -0,0 +1,20 @@
+package java8;
+
+public class Day0613 {
+	public static void main(String[] args) {
+		int i;
+		for (i=1; i<= 10; i++) {
+			if(i%2 != 0) {
+				continue;
+			}
+		System.out.println(i);
+		
+		// if 홀수라면 continue 해라.
+		// 홀수면 계속 하고 짝수면 false 라서 멈춘다. 짝수면 출력한다.
+		//continue 대신 break; 를 쓰면 아무것도 출력되지 않을거다.
+		// continue = 현재 실행되고 있는 회차만 종료하고 다음 번으로 넘어가는 것.
+		
+		
+		}
+	}
+}
