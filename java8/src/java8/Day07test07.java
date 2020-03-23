56f2e6c81602102583bff24fad585b1ee6a186fc
SinJaeWon <yn-s02@DESKTOP-LGKMUIJ>
2020-03-23 11:02:45 +0900

    자바7일차_1~50 3의 배수의 개수

---
+++ java8/src/java8/Day07test07.java
@@ -0,0 +1,29 @@
+package java8;
+
+public class Day07test07 {
+
+	//1부터 50까지의 숫자 중 3의 배수의 개수를 구하라.
+	public static void main(String[] args) {
+	//갯수는 count를 쓴다.
+		
+		int cnt = 0;
+		
+		for(int i = 1; i <= 50; i++) {
+			if(i % 3 == 0) {
+				cnt++;
+			}
+		}
+		System.out.println("1부터 50까지 중 3의 배수의 개수는? : " + cnt);
+		
+//		int sum = 0;
+//		
+//		for(int i = 0; i <=50; i++) {
+//			if(i%3!=0) {
+//				continue;
+//			}
+//		}
+//		
+//		System.out.println("3의 배수의 개수는?: ");
+	}
+	
+}
