8d4c5db59a0cc1287f851d76032adbf5dffb56f3
SinJaeWon <shjaewon.dev@gmail.com>
2020-03-25 16:30:37 +0900

    자바9일차: 숙제 for문으로 알파벳 출력

---
+++ java8/src/java8/Day0905.java
@@ -0,0 +1,16 @@
+package java8;
+
+public class Day0905 {
+	//형변환이 필요하다.
+	public static void main(String[] args) {
+		
+		char a = 0;
+		char b = 0;
+		
+		for(int  i = 1; i <=26; i++) {
+			System.out.print(a);
+		}
+		
+	}
+	
+}
