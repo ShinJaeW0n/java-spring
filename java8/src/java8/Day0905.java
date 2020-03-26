2b2376df96c00db1924ac4a7f375792bbaab6571
SinJaeWon <shjaewon.dev@gmail.com>
2020-03-26 09:47:55 +0900

    자바9일차: for문으로 a~z 출력하기

--- java8/src/java8/Day0905.java
+++ java8/src/java8/Day0905.java
@@ -4,12 +4,45 @@ public class Day0905 {
 	//형변환이 필요하다.
 	public static void main(String[] args) {
 		
-		char a = 0;
-		char b = 0;
 		
-		for(int  i = 1; i <=26; i++) {
-			System.out.print(a);
+//		char ch = 'a'; //형변환
+//
+//		for (int i = 1; i <= 26; i++) {
+//
+//			System.out.print(ch++ + " "); // ?
+//
+//			
+//
+//		}System.out.println();
+//
+//		
+//		
+//		char ch1 = 'A';
+//
+//		for (int i = 1; i <= 26; i++) {
+//
+//			System.out.print(ch1++ + " "); 
+//
+//			
+//
+//		}System.out.println();
+		
+		
+		
+		//포문에서 1씩 증가시켜서 하나씩 출력해볼 수 있음.
+		//쌤이 한거 	97		   122를 넣어도 똑같이 나옴.
+		for(int i = 'a'; i <= 'z'; i++) {
+			System.out.print((char)i + " ");//형변환
 		}
+		
+		System.out.println();
+		
+		
+		for(int i = 'A'; i <= 'Z'; i++) {
+			System.out.print((char)i + " ");
+		}
+		
+		System.out.println();
 		
 	}
 	
