e81bf5f897153815592ab33151a1a087d2d55e31
SinJaeWon <shjaewon.dev@gmail.com>
2020-03-31 14:54:26 +0900

    자바13일차;

---
+++ java8/src/Day12/Ex2.java
@@ -0,0 +1,18 @@
+package Day12;
+
+public class Ex2 { //정렬할때 사용.
+			public static void main(String[] args) {
+				String a = "aaa";
+				String b = "Abc";
+				//aaa			aab						abc
+		    // 97 97 97		   97 97 98				  97 98 99
+				//97-98 = -1 , 그래서 결과가 -1이 나온다.
+				//같으면 다음 결과로 넘어가고 같지 않다면 거기서 2개의 값의 차이를 비교하고 멈춘다. 그 다음으로 넘어가지 않는다.
+				//양수가 나오면 숫자가 앞쪽이고 음수면 숫자가 그 다음순서다.
+				//정렬할때 앞 뒤 숫자 비교할때 오름차순, 내림차순 배웠는데 compareTo로 정렬해줄 수 있다.
+				//값이 얼마 나오는진 중요하지 않다.
+				int res = a.compareTo(b);
+				System.out.println("비교결과: " + res);
+				
+	}
+}
