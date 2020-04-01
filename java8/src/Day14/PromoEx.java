8969ac6968838597387f679197e93b09d41cdb49
SinJaeWon <shjaewon.dev@gmail.com>
2020-04-01 15:23:36 +0900

    자바 14일차: 형변환***

--- java8/src/Day14/PromoEx.java
+++ java8/src/Day14/PromoEx.java
@@ -16,9 +16,27 @@ public class PromoEx {
 //			pc = pp;  //자식에 부모를 대입할 수 없습니다.
 				
 			
-			//강제 형변환
+			//강제 형변환 , 부모의 형태에서 자식으로 바뀌면 자식메소드를 호출할 수 있다.
 			pc = (PromoChild)pp; // <- 강제로 자식에 부모를 대입할 수 있다.
+			pc.method1();	//부모의 객체가 자식의 객체에 대입될 때에는 원래부터 자식 객체였어야만 합니다.
+			pc.method2();
+			pc.method3();
 			
+			
+			PromoParent pp2 = new PromoParent();
+					
+					//instanceof ->자료의 형태를 확인할때 사용, 형변환이 가능한지 boolean 값으로 반환합니다. 좌(객체) 우(타입)
+			if (pp instanceof PromoChild) {
+				pc = (PromoChild)pp;
+				pc.method3();
+			}else {
+				System.out.println("강제형변환을 사용할 수 없습니다.");
+			}
+			
+			
+//			pc = (PromoChild)pp2;
+//			pc.method3(); // <- 강제형변환은 원래의 형태로 복구만 가능합니다. pp2가 프로모차일드 형이 아니기 때문에 출력이 안됨.
+//			
 			
 	}
 
